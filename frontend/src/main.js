3299ef46cf83d13bdf7ac1a79c5949b5780e618e
Daniel Hillebrand <daniel.hillebrand@alumni.fh-aachen.de>
2020-12-05 20:25:06 +0100

    adds datetimepicker plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate/src';
+import DatetimePicker from 'vuetify-datetime-picker';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -8,6 +9,7 @@ import vuetify from './plugins/vuetify';
 Vue.config.productionTip = false;
 
 Vue.use(Vuelidate);
+Vue.use(DatetimePicker);
 
 new Vue({
   router,
