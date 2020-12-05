6c3d400c0850c41cab1dbf9f81b97bca42000a14
RoB - Robert Bagdahn <rob@ROB-MACBOOK.local>
2020-12-05 13:02:16 +0100

    added event model

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# init commit 
+`pipenv shell``
+
+pipenv 
+
---
+++ backend/basic/serializers.py
@@ -0,0 +1,15 @@
+# serializers.py
+from rest_framework import serializers
+from .models import Event
+
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Event
+        fields = (
+            'id',
+            'name',
+            'description',
+            'caro'
+        )
