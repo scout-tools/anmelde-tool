21b8648f1a60973333f98db98f044c5254dab57d
Philip Hagemann <philip.hagemann@tum.de>
2020-12-08 00:32:33 +0100

    -Bugfix in extended usermodel where the model is not created when a new user is created

--- backend/anmeldung_auth/models.py
+++ backend/anmeldung_auth/models.py
@@ -9,8 +9,12 @@ class UserExtended(models.Model):
     password_date = models.DateTimeField(auto_now=True)
 
 
-@receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
-def save_profile(sender, instance, created, **kwargs):
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserExtended.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
     instance.userextended.save()
-
-
