58299d779149a431f005d5125b81383c2eb0d200
Philip Hagemann <philip.hagemann@tum.de>
2020-12-14 19:48:24 +0100

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '83.169.2.26',
