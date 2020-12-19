f4f9328fe8eab6c2392b3a58816311f57f844bd7
Daniel Hillebrand <daniel.hillebrand@alumni.fh-aachen.de>
2020-12-19 21:20:11 +0100

    refactors step arrays

--- frontend/src/views/event/create/Main.vue
+++ frontend/src/views/event/create/Main.vue
@@ -1,40 +1,40 @@
 <template>
   <v-card max-width="600" class="mx-auto">
     <v-row justify="center">
-        <v-stepper
-          v-model="currentStep"
-          vertical
-        >
-          <template v-for="(step, index) in steps">
-            <v-stepper-step
-              :key="`stepper-${index}`"
-              :complete="currentStep > index+1"
+      <v-stepper
+        v-model="currentStep"
+        vertical
+      >
+        <template v-for="(step, index) in steps">
+          <v-stepper-step
+            :key="`stepper-${index}`"
+            :complete="currentStep > index+1"
+            :step="index+1"
+          >
+            {{ headerSteps[index] }}
+          </v-stepper-step>
+
+          <v-divider
+            :key="index"
+          ></v-divider>
+
+          <v-stepper-items :key="`stepper-items-${index}`">
+            <v-stepper-content
               :step="index+1"
             >
-              {{ headerStep[index] }}
-            </v-stepper-step>
-
-            <v-divider
-              :key="index"
-            ></v-divider>
-
-            <v-stepper-items :key="`stepper-items-${index}`">
-              <v-stepper-content
-                :step="index+1"
-              >
-                <component
-                  :is="step"
-                  :ref="step.name"
-                  :position="index+1"
-                  :max-pos="steps.length"
-                  @prevStep="prevStep()"
-                  @nextStep="nextStep()"
-                  @submit="onCreateEventClick()"
-                />
-              </v-stepper-content>
-            </v-stepper-items>
-          </template>
-        </v-stepper>
+              <component
+                :is="step"
+                :ref="step.name"
+                :position="index+1"
+                :max-pos="steps.length"
+                @prevStep="prevStep()"
+                @nextStep="nextStep()"
+                @submit="onCreateEventClick()"
+              />
+            </v-stepper-content>
+          </v-stepper-items>
+        </template>
+      </v-stepper>
     </v-row>
     <v-snackbar
       v-model="showSuccess"
@@ -76,23 +76,34 @@ export default {
     return {
       API_URL: process.env.VUE_APP_API,
       currentStep: 1,
-      steps: [StepNameDescription, StepStartEndDeadline, StepLocation, StepAgeGroup, StepEventContact], // eslint-disable-line max-len
       showError: false,
       showSuccess: false,
       timeout: 7000,
-      headerStep: [
-        'Beschreibung',
-        'Daten und Uhrzeit',
-        'Ort',
-        'Zielgruppe',
-        'Kontaktdaten',
-      ],
       data: {
         event: {},
       },
     };
   },
-
+  computed: {
+    steps() {
+      return [
+        StepNameDescription,
+        StepStartEndDeadline,
+        StepLocation,
+        StepAgeGroup,
+        StepEventContact,
+      ];
+    },
+    headerSteps() {
+      return [
+        'Beschreibung',
+        'Daten und Uhrzeit',
+        'Ort',
+        'Zielgruppe',
+        'Kontaktdaten',
+      ];
+    },
+  },
   methods: {
     nextStep() {
       this.currentStep += 1;
