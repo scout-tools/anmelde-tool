281777d71496631e2394055f73e0c3abfc672aeb
Paul Kruse <paul.kruse@posteo.de>
2020-12-28 12:04:40 +0100

    Added participants list endpoint with some more information than in the participant table

--- backend/basic/views.py
+++ backend/basic/views.py
@@ -1,9 +1,12 @@
 # views.py
+from itertools import chain
+from rest_framework.response import Response
 from django.db.models.functions import ExtractWeek, ExtractYear
 from rest_framework import pagination, viewsets, mixins, generics, filters
 
 from .models import Event, AgeGroup, EventLocation, ScoutHierarchy, Registration, ZipCode, Participants
-from .serializers import EventSerializer, AgeGroupSerializer, EventLocationSerializer, ScoutHierarchySerializer, RegistrationSerializer, ZipCodeSerializer, ParticipantsSerializer
+from .serializers import EventSerializer, AgeGroupSerializer, EventLocationSerializer, ScoutHierarchySerializer, \
+    RegistrationSerializer, ZipCodeSerializer, ParticipantsSerializer
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -36,6 +39,29 @@ class ZipCodeViewSet(viewsets.ModelViewSet):
     serializer_class = ZipCodeSerializer
 
 
+def get_bund_name(scout_organisation):
+    if scout_organisation.level_id > 3:
+        return get_bund_name(scout_organisation.parent)
+    elif scout_organisation.level_id < 3:
+        raise Exception("To low value")
+    else:
+        return scout_organisation.name
+
+
 class ParticipantsViewSet(viewsets.ModelViewSet):
     queryset = Participants.objects.all()
     serializer_class = ParticipantsSerializer
+
+    def list(self, request, event_pk=None):
+        registrations = Registration.objects.all().filter(event_id=event_pk)
+        data = []
+        for reg in registrations:
+            participants = reg.participants_set.all()
+            for participant in participants:
+                participant_data = self.serializer_class(participant).data
+                bund_name = get_bund_name(reg.scout_organisation)
+                participant_data["bund"] = bund_name
+                participant_data["lat"] = reg.scout_organisation.zip_code.lat
+                participant_data["lon"] = reg.scout_organisation.zip_code.lon
+                data.append(participant_data)
+        return Response(data)
