350b3c12100aab3c28506a05d075a5b3d95fec26
Philip Hagemann <philip.hagemann@tum.de>
2021-01-19 21:34:37 +0100

    -Added test data for eathabit -Added test data for participantpersonel -added a django command for aws to collect static data -Added permission classes to check whether certain views (Kitchen,logistic,socialmedia,program,cash and being a responsible person) -Added serializers and view for kitchen,program and cash, also for participants of events and groups -Extended EventOverview by two values: does the auth user has a role and her organisation registered -Added slug for EatHabitType and to check whether a type exists and add if necessary -Added required urls -Changed some rights in views,

---
+++ backend/basic/permissions.py
@@ -0,0 +1,56 @@
+from rest_framework import permissions
+from .models import EventRoleMapping, Registration
+
+
+def get_eventrole(request, view, event_role_id):
+    return EventRoleMapping.objects.filter(event_id=view.kwargs.get("event_pk", None),
+                                           user=request.user, event_role_id__in=event_role_id).exists()
+
+
+class IsEventMaster(permissions.BasePermission):
+    message = 'Nur für die Lagerleitung.'
+
+    def has_permission(self, request, view):
+        return get_eventrole(request, view, [1, ])
+
+
+class IsEventCashMaster(permissions.BasePermission):
+    message = 'Nur für das Kassenteam oder Lagerleitung.'
+
+    def has_permission(self, request, view):
+        return get_eventrole(request, view, [1, 2])
+
+
+class IsKitchenMaster(permissions.BasePermission):
+    message = 'Nur für das Kücheteam oder Lagerleitung.'
+
+    def has_permission(self, request, view):
+        return get_eventrole(request, view, [1, 3])
+
+
+class IsProgramMaster(permissions.BasePermission):
+    message = 'Nur für das Programmtean oder Lagerleitung.'
+
+    def has_permission(self, request, view):
+        return get_eventrole(request, view, [1, 4])
+
+
+class IsLogisticMaster(permissions.BasePermission):
+    message = 'Nur für das Logistikteam oder Lagerleitung.'
+
+    def has_permission(self, request, view):
+        return get_eventrole(request, view, [1, 5])
+
+
+class IsSocialMediaPermission(permissions.BasePermission):
+    message = 'Nur für das Öffentlichkeitsteam oder Lagerleitung.'
+
+    def has_permission(self, request, view):
+        return get_eventrole(request, view, [1, 6])
+
+
+class IsResponsiblePersonPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return Registration.objects.filter(id=view.kwargs.get("registration_pk"),
+                                           responsible_persons=request.user).exists()
