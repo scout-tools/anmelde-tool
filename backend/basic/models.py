bafd24123478b19dd5ee01cc4aaee1df29cdd50b
Philip Hagemann <philip.hagemann@tum.de>
2021-01-24 18:32:13 +0100

    Reworked participant view

--- backend/basic/models.py
+++ backend/basic/models.py
@@ -4,8 +4,8 @@ from rest_framework.renderers import JSONRenderer
 
 
 class TimeStampMixin(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     class Meta:
         abstract = True
