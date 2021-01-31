c328ae6bd257e9f1a0ab3f13034667104b8528cf
RoB - Robert Bagdahn <rob@ROB-MACBOOK.local>
2021-01-31 11:05:05 +0100

    new stammesdaten

--- test-data-generator/convert_all_stamm.py
+++ test-data-generator/convert_all_stamm.py
@@ -10,18 +10,18 @@ with open('output/0_zip_codes.json') as json_file:
 
 
 def findIdByCity(plz):
-    print(plz)
+
     for i, event in enumerate(zipCodes, start=0):
         if (zipCodes[i]['fields']['zip_code'] == plz):
             return zipCodes[i]['fields']['id']
     return 1
 
 
-input_file = csv.DictReader(open('input/bdp_stammesliste.csv'), fieldnames=[
-                            'id', 'bezirk', 'ring_name', 'stamm', 'city'], delimiter=";")
+input_file = csv.DictReader(open('input/stamm_gesamt.csv'), fieldnames=[
+                            'stamm', 'parent_id', 'plz'], delimiter=";")
 
 for i, event in enumerate(input_file, start=1):
-    ii = i + 41
+    ii = i + 81
     result.append({
         "model": model,
         "pk": ii,
@@ -31,8 +31,8 @@ for i, event in enumerate(input_file, start=1):
     result[i-1]['fields']['id'] = ii
     result[i-1]['fields']['name'] = event['stamm']
     result[i-1]['fields']['zip_code'] = findIdByCity(event['plz'])
-    result[i-1]['fields']['parent_id'] = 1
+    result[i-1]['fields']['parent_id'] = int(event['parent_id'], 10)
     result[i-1]['fields']['level_id'] = 5
 
-with open('output/0_bdp.json', 'w') as outfile:
+with open('output/all_stamm.json', 'w') as outfile:
     json.dump(result, outfile)
