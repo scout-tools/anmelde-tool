e3feb184d9464e4452422196d4d6ce12bcc9e7fc
Philip Hagemann <philip.hagemann@tum.de>
2021-02-28 22:50:34 +0100

    Added Responsibleperson checks for going live

--- backend/basic/views.py
+++ backend/basic/views.py
@@ -205,7 +205,7 @@ class ScoutOrgaLevelViewSet(viewsets.ModelViewSet):
 
 
 class ParticipantPersonalViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]  # ToDo: implement IsResponsiblePersonPermission
+    permission_classes = [IsAuthenticated,IsResponsiblePersonPermission]
     queryset = ParticipantPersonal.objects.all()
     serializer_class = ParticipantPersonalSerializer
 
@@ -345,7 +345,7 @@ class EventParticipantsViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class RegistrationParticipantsViewSet(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [IsAuthenticated]  # ToDo: implement IsResponsiblePersonPermission
+    permission_classes = [IsAuthenticated,IsResponsiblePersonPermission]
     serializer_class = RegistrationParticipantsSerializer
 
     def get_queryset(self):
@@ -357,7 +357,7 @@ class RegistrationParticipantsViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class RegistrationSummaryViewSet(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [IsAuthenticated]  # ToDo: implement IsResponsiblePersonPermission
+    permission_classes = [IsAuthenticated,IsResponsiblePersonPermission]
     serializer_class = RegistrationSummarySerializer
 
     def get_queryset(self):
