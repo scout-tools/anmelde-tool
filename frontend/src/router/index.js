8abfeead2e06be8e5dfee8e25851bb47141284fe
Daniel Hillebrand <daniel.hillebrand@alumni.fh-aachen.de>
2021-04-05 12:21:57 +0200

    adds dummy page to pdf functionality

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,6 +12,8 @@ import LoginParticipantsMain from '@/views/login/LoginParticipants.vue';
 import LoginInteralsMain from '@/views/login/LoginInterals.vue';
 import Impressum from '@/views/footer/Impressum.vue';
 import Datenschutz from '@/views/footer/Datenschutz.vue';
+import pdfGeneration from '@/views/pdfGeneration/Main.vue';
+import pdfContent from '../views/pdfGeneration/PdfContent.vue';
 
 Vue.use(VueRouter);
 
@@ -20,6 +22,16 @@ const routes = [
     path: '/',
     name: 'landing',
     component: LandingPage,
+  },
+  {
+    path: '/pdf',
+    name: 'pdf',
+    component: pdfContent,
+  },
+  {
+    path: '/generate-pdf',
+    name: 'generatePdf',
+    component: pdfGeneration,
   },
   {
     path: '/login-participants',
