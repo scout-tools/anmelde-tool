cae3c033e7c80476a3401e55e099d99029a3752c
Daniel Hillebrand <daniel.hillebrand@alumni.fh-aachen.de>
2021-04-05 23:37:56 +0200

    removes vue-html2pdf and finish basic functionality with html2pdf.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,6 @@ import VueGoogleCharts from 'vue-google-charts';
 import VueLodash from 'vue-lodash';
 import lodash from 'lodash';
 import AsyncComputed from 'vue-async-computed';
-import VueHtml2pdf from 'vue-html2pdf';
 
 import App from './App.vue';
 import router from './router';
@@ -23,7 +22,6 @@ Vue.use(VueJwtDecode);
 Vue.use(VuetifyMoney);
 Vue.use(VueGoogleCharts);
 Vue.use(AsyncComputed);
-Vue.use(VueHtml2pdf);
 
 Vue.use(VueLodash, { lodash });
 
