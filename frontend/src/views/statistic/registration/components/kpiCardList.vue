a3764cd5ab78369d7f9bc24ff89cb1f0e1b421e7
RoB - Robert Bagdahn <rob@ROB-MACBOOK.local>
2021-04-18 09:15:10 +0200

    add kpi dashboarf

---
+++ frontend/src/views/statistic/registration/components/kpiCardList.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="mx-auto" :color="color" dark max-width="400">
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline">{{ data.header }}</v-list-item-title>
+        <v-list-item-subtitle>{{ data.subheader }}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item dense v-for="(item, index) in data.dataOne" :key="index">
+      <v-list-item-icon>
+        <v-icon>mdi-account-group</v-icon>
+      </v-list-item-icon>
+      <v-list-item-subtitle>{{ item.scoutOrganisation}} </v-list-item-subtitle>
+    </v-list-item>
+
+    <!-- <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn text> Full Report </v-btn>
+    </v-card-actions> -->
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['data', 'color'],
+  data: () => ({
+  }),
+};
+</script>
