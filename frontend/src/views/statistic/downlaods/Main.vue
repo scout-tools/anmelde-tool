a2f8ade3c4c75df3b9c5dca7b325b5c4768e6aa0
RoB - Robert Bagdahn <rob@ROB-MACBOOK.local>
2021-04-18 20:23:56 +0200

    added download buttons

---
+++ frontend/src/views/statistic/downlaods/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-tabs vertical icons-and-text class="pa-0">
+    <v-tab>
+      <v-icon left> mdi-microsoft-excel </v-icon>
+      Excel
+    </v-tab>
+    <v-tab class="pa-0">
+      <v-icon left> mdi-file-pdf </v-icon>
+      PDFs
+    </v-tab>
+
+    <v-tab-item>
+      <v-card flat>
+        <v-card-text>
+          <file-download />
+        </v-card-text>
+      </v-card>
+    </v-tab-item>
+
+    <v-tab-item>
+      <v-card flat>
+        <v-card-text>
+          <pdf-generation-interface />
+        </v-card-text>
+      </v-card>
+    </v-tab-item>
+  </v-tabs>
+</template>
+
+<script>
+import PdfGenerationInterface from './PdfGenerationInterface.vue';
+import FileDownload from './FileDownload.vue';
+
+export default {
+  components: {
+    PdfGenerationInterface,
+    FileDownload,
+  },
+};
+</script>
