f10d314bcff73018e9d6b59eb768c186f369c056
Robert Bagdahn <rob@vnr.de>
2021-07-23 23:30:54 +0200

    removed https

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = [
     'anmeldetool-backend-dev.eba-sb52mfq3.eu-central-1.elasticbeanstalk.com',
     'anmeldetoolbackenddev-prod.eu-central-1.elasticbeanstalk.com',
     '1agdtsauoj.execute-api.eu-central-1.amazonaws.com',
-    'https://oev1ba563c.execute-api.eu-central-1.amazonaws.com/devbusife',
+    'oev1ba563c.execute-api.eu-central-1.amazonaws.com',
     'dev.api.anmelde-tool.de',
     'api.anmelde-tool.de'
 ]
