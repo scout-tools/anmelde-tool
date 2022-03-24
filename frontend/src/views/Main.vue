a5116120e3531dc1bf90d535ab82ee87c8491231
Robert Bagdahn <robertbaggi@gmail.com>
2022-03-24 19:26:36 +0100

    fixed breakpoint

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -30,7 +30,7 @@ export default {
   },
   computed: {
     isMobile() {
-      return !this.$vuetify.breakpoint.md;
+      return !this.$vuetify.breakpoint.md && !this.$vuetify.breakpoint.lg;
     },
   },
   mounted() {
