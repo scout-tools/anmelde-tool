3ece10a634a35eaafa3e51711f9609e5c22a1cfa
Philip Hagemann <Philip.Hagemann@partner.bmw.de>
2022-03-27 17:45:00 +0200

    * improved some things

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -203,7 +203,7 @@ if env.bool('USE_SES', False):
 # SQS Settings
 AWS_EB_RUN_TASKS_LOCALLY = not env.bool('USE_SQS', False)
 AWS_EB_DEFAULT_REGION = 'eu-central-1'
-AWS_EB_DEFAULT_QUEUE_NAME = "anmelde-tool-queue"
+AWS_EB_DEFAULT_QUEUE_NAME = env('AWS_EB_DEFAULT_QUEUE_NAME')
 AWS_EB_HANDLE_SQS_TASKS = env.bool('AWS_EB_HANDLE_SQS_TASKS', False)
 
 REST_USE_JWT = True
