02ae9d59f2e9dd52d770852b9e4049a4d500f53c
Philip Hagemann <philip.hagemann@tum.de>
2022-05-27 23:02:17 +0200

    * undo change in linebreak style

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
-    'linebreak-style': 0,
+    'linebreak-style': ['error', 'unix'],
   },
 };
