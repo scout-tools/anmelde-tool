82b1244fc6c5281c33de4643c7834945210ac092
Robert Bagdahn <robertbaggi@gmail.com>
2022-07-17 13:07:29 +0200

    add TravelAttributeV2 to statistics

---
+++ frontend/src/views/statistic/attributes/tabs/TravelAttributeV2.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-row class="center text-center justify-center">
+      <v-card class="mx-auto pa-0" flat>
+        <v-container class="pa-0" fluid>
+          <v-card-title>
+            {{ attribute.title }}
+          </v-card-title>
+          <v-card-subtitle>
+            {{ attribute.text }}
+          </v-card-subtitle>
+        </v-container>
+      </v-card>
+    </v-row>
+    <v-row justify="center" class="overflow-y: auto">
+      <v-data-table
+        :headers="headers"
+        :items="getItems"
+        :items-per-page="itemsPerPage"
+        hide-default-footer
+        item-key="id">
+        <template v-slot:[`item.tag.dateTimeField`]="{ item }">
+          {{ formatDate(item) }}
+        </template>
+      </v-data-table>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  props: ['attribute'],
+  data: () => ({
+    headers: [
+      {
+        text: 'Stamm',
+        value: 'registration.scoutOrganisation.name',
+      },
+      {
+        text: 'Datum',
+        value: 'tag.dateTimeField',
+      },
+      {
+        text: 'Anreisemethode',
+        value: 'tag.typeField',
+      },
+      {
+        text: 'Anzahl Personen',
+        value: 'tag.numberPersons',
+      },
+      {
+        text: 'Beschreibung',
+        value: 'tag.description',
+      },
+    ],
+    API_URL: process.env.VUE_APP_API,
+    itemsPerPage: 1000,
+  }),
+  computed: {
+    eventId() {
+      return this.$route.params.id;
+    },
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated;
+    },
+    getItems() {
+      return this.attribute.attributes.data;
+    },
+  },
+
+  methods: {
+    formatDate(item) {
+      return this.$moment(item.tag.dateTimeField)
+        .format('DD.MM.YYYY HH:mm');
+    },
+  },
+};
+</script>
